252089fd0d00b3340cfda88cc019c9926d7de860
savioquixaba <savioquixaba1@hotmail.com>
2026-03-11 10:12:03 -0300

    feat: adiciona Notations do Lombok na Classe Transação

--- src/main/java/dev/Java10x/itau/Java10x/Transacao.java
+++ src/main/java/dev/Java10x/itau/Java10x/Transacao.java
@@ -1,8 +1,15 @@
 package dev.Java10x.itau.Java10x;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transacao {
 
     private BigDecimal valor;
