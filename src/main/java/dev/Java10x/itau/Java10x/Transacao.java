eb35340eb7a8f42f547575f089dfa97fe7582a59
savioquixaba <savioquixaba1@hotmail.com>
2026-03-11 09:56:42 -0300

    initial commit

---
+++ src/main/java/dev/Java10x/itau/Java10x/Transacao.java
@@ -0,0 +1,10 @@
+package dev.Java10x.itau.Java10x;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class Transacao {
+
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+}
---
+++ src/test/java/dev/Java10x/itau/Java10x/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Java10x.itau.Java10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
