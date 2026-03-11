e74ccc3b91e7da79b5c327219b9da11e46bfe2fe
savioquixaba <savioquixaba1@hotmail.com>
2026-03-11 10:29:32 -0300

    feat: Adiciona Controller da Transação e cria ResponseEntity do Endpoint POST para retornar Status Correto ao ser requisitado

---
+++ src/main/java/dev/Java10x/itau/Java10x/TransacoesController.java
@@ -0,0 +1,23 @@
+package dev.Java10x.itau.Java10x;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacoesController {
+
+    @PostMapping
+    public ResponseEntity<Transacao> adicionar(){
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+
+
+
+}
