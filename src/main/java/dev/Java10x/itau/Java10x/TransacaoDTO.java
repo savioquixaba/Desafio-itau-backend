a57f1761a04f9d4aedae1f4231eee14c30424b8f
savioquixaba <savioquixaba1@hotmail.com>
2026-03-11 19:19:04 -0300

    feat: Cria DTO da Transação para tirar responsabilidade do Model

---
+++ src/main/java/dev/Java10x/itau/Java10x/TransacaoDTO.java
@@ -0,0 +1,17 @@
+package dev.Java10x.itau.Java10x;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransacaoDTO {
+
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+}
