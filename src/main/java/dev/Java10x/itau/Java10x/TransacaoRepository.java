7aae0242a698b0e7184bb4a25b12c5bae041e5b6
savioquixaba <savioquixaba1@hotmail.com>
2026-03-12 13:59:29 -0300

    feat: Cria Repositorio para armazenar Lista e metodos de persistencia

---
+++ src/main/java/dev/Java10x/itau/Java10x/TransacaoRepository.java
@@ -0,0 +1,24 @@
+package dev.Java10x.itau.Java10x;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class TransacaoRepository {
+
+    List<TransacaoDTO> listaDeTransacoes = new ArrayList<>();
+    //salvar dados em uma lista
+    public void salvarDados(TransacaoDTO transacaoDTO){
+
+        listaDeTransacoes.add(transacaoDTO);
+    }
+    //apagar essa lista depois 60 segundos
+    public void limparDados(TransacaoDTO transacaoDTO){
+
+    }
+    //deletar dados
+    public void deletarDados(){
+        listaDeTransacoes.clear();
+    }
+}
