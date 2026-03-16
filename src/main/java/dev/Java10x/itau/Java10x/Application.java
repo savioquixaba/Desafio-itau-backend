198d72c6dc5fad9af222ba5ede33f714108fd009
savioquixaba <savioquixaba1@hotmail.com>
2026-03-16 16:59:11 -0300

    chore: adicionado notacao do spring para os segundos da estatistica ser gerenciado no spring dentro do main

--- src/main/java/dev/Java10x/itau/Java10x/Application.java
+++ src/main/java/dev/Java10x/itau/Java10x/Application.java
@@ -2,8 +2,10 @@ package dev.Java10x.itau.Java10x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
 	public static void main(String[] args) {
