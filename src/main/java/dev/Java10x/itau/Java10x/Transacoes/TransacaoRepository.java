4a35d9c5d313a0a6ec84ddaaccc7c1e589360a18
savioquixaba <savioquixaba1@hotmail.com>
2026-03-16 16:58:01 -0300

    feat: cria metodo responsável pelas validações da lista de estatisticas

--- src/main/java/dev/Java10x/itau/Java10x/Transacoes/TransacaoRepository.java
+++ src/main/java/dev/Java10x/itau/Java10x/Transacoes/TransacaoRepository.java
@@ -1,13 +1,19 @@
 package dev.Java10x.itau.Java10x.Transacoes;
 
+import dev.Java10x.itau.Java10x.Estatisticas.EstatisticaDTO;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 @Repository
 public class TransacaoRepository {
 
+
     List<TransacaoDTO> listaDeTransacoes = new ArrayList<>();
+
+
+
     //salvar dados em uma lista
     public void salvarDados(TransacaoDTO transacaoDTO){
 
@@ -20,5 +26,19 @@ public class TransacaoRepository {
     //deletar dados
     public void deletarDados(){
         listaDeTransacoes.clear();
+    }
+
+    //GERAR ESTATISTICAS
+
+    public EstatisticaDTO estatistica(OffsetDateTime horaInicial){
+        if (listaDeTransacoes.isEmpty()){
+            return new EstatisticaDTO(0,0.0,0.0,0.0,0.0);
+        }
+        final var summary = listaDeTransacoes.stream()
+                .filter(t -> t.getDataHora().isAfter(horaInicial) || t.getDataHora().isEqual(horaInicial))
+                .mapToDouble(t -> t.getValor().doubleValue())
+                .summaryStatistics();
+
+        return new EstatisticaDTO(summary.getCount(),summary.getAverage(),summary.getMax(), summary.getMin(), summary.getSum());
     }
 }
