4e166ac695e9fa4095fc8971814820d88990a959
savioquixaba <savioquixaba1@hotmail.com>
2026-03-16 16:56:39 -0300

    feat: cria controller responsavel pelo metodo GET das estatisticas

---
+++ src/main/java/dev/Java10x/itau/Java10x/Estatisticas/EstatisticasController.java
@@ -0,0 +1,32 @@
+package dev.Java10x.itau.Java10x.Estatisticas;
+
+import dev.Java10x.itau.Java10x.Transacoes.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/estatistica")
+public class EstatisticasController {
+
+
+    @Autowired
+    private EstatisticaProperties estatisticaProperties;
+
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
+    //criar Rota de estatistica e uma logica para trabalhar com dados
+    @GetMapping
+    public ResponseEntity estatisticas() {
+
+        final var horaInicial = OffsetDateTime.now().minusSeconds(estatisticaProperties.segundos());
+
+        return ResponseEntity.ok(transacaoRepository.estatistica(horaInicial));
+    }
+}
