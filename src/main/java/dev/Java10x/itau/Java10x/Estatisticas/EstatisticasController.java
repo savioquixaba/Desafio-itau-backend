c07cdee21518ad10b827484d8e59c7d2c33cbf9a
savioquixaba <savioquixaba1@hotmail.com>
2026-03-18 09:50:27 -0300

    feat: Adiciona logs de controle na aplicação

--- src/main/java/dev/Java10x/itau/Java10x/Estatisticas/EstatisticasController.java
+++ src/main/java/dev/Java10x/itau/Java10x/Estatisticas/EstatisticasController.java
@@ -1,6 +1,7 @@
 package dev.Java10x.itau.Java10x.Estatisticas;
 
 import dev.Java10x.itau.Java10x.Transacoes.TransacaoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
-
+@Slf4j
 @RestController
 @RequestMapping("/estatistica")
 public class EstatisticasController {
@@ -24,7 +25,8 @@ public class EstatisticasController {
     //criar Rota de estatistica e uma logica para trabalhar com dados
     @GetMapping
     public ResponseEntity estatisticas() {
-
+        //log de requisição criado via lombok
+        log.info("Calculando estatistica de transações : " + estatisticaProperties.segundos());
         final var horaInicial = OffsetDateTime.now().minusSeconds(estatisticaProperties.segundos());
 
         return ResponseEntity.ok(transacaoRepository.estatistica(horaInicial));
--- src/main/java/dev/Java10x/itau/Java10x/Transacoes/TransacoesController.java
+++ src/main/java/dev/Java10x/itau/Java10x/Transacoes/TransacoesController.java
@@ -1,10 +1,12 @@
 package dev.Java10x.itau.Java10x.Transacoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/transacao")
 public class TransacoesController {
@@ -21,6 +23,7 @@ public class TransacoesController {
             transacaoRepository.salvarDados(transacaoDTO);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }catch (IllegalArgumentException exception){
+            log.error("Erro em uma ou mais validações, tente novamente!");
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
